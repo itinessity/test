878f12252d253edb4e14ab772214efc3acaaa73e
Eugenia_Tamasyan <itinessity@gmail.com>
2024-01-14 19:26:39 -0600

    version update

---
+++ src/pages/projects/comics/comics6.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-comics-6',
+  templateUrl: './comics6.html',
+  styleUrls: ['./comics6.css']
+})
+export class Comics6Component {
+
+}
